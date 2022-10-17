e000855bf6a95982124f30d1391801deb99f2317
schungzhihuai <41113285+schungzhihuai@users.noreply.github.com>
2022-10-17 22:49:35 +0800

    landing page update

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -7,15 +7,15 @@ import { RouterLink } from 'vue-router'
   <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
   <nav class="navbar sticky-top navbar-expand-md px-4 py-3 border-bottom">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Property Planners</a>
+      <a class="navbar-brand" href="#">PropertyPlanners</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
         aria-controls="offcanvasNavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-          <a class="navbar-brand" href="#">Property Planners</a>
+        <div class="offcanvas-header border-bottom">
+          <a class="navbar-brand" href="#">PropertyPlanners</a>
           <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
