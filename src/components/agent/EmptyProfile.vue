0a2795dd6f5f7520dba80d561ac9ab80b1d6d7aa
Samuel Chung <41113285+schungzhihuai@users.noreply.github.com>
2022-11-09 02:21:11 +0800

    create api for agent data

--- src/components/agent/EmptyProfile.vue
+++ src/components/agent/EmptyProfile.vue
@@ -3,20 +3,12 @@
     <p class="pp-head text-start">My Profile</p>
     <div class="flex-row d-flex justify-content-center">
       <div class="border-0 text-center">
-        <img
-          class="p-3 mx-auto illustration"
-          src="../../assets/GetVerified.svg"
-          alt="Empty Dashboard"
-        />
+        <img class="p-3 mx-auto illustration" src="../../assets/GetVerified.svg" alt="Empty Dashboard" />
         <p class="pp-subhead my-3 mx-auto" id="ill-subhead">
           Get verified and have your property agent data automatically imported
         </p>
         <!-- <button class="btn btn-success mx-auto">Find Agents</button> -->
-        <button
-          type="button"
-          class="mx-auto btn btn-primary rounded-pill btn-lg pp-button"
-          v-on:click="test()"
-        >
+        <button type="button" class="mx-auto btn btn-primary rounded-pill btn-lg pp-button" v-on:click="getAgentData()">
           Verify
         </button>
       </div>
@@ -25,14 +17,28 @@
 </template>
 
 <script>
+import { AgentData } from '../../scripts/agentdata'
+
 
 export default {
-    name: 'EmptyAgentProfile',
-    methods:{
-        test(){
-            console.log(this.$route)
-        }
+  name: 'EmptyAgentProfile',
+  data() {
+    return {
+      agent_registration_no: 'R064253E'
     }
+  },
+  methods: {
+    test() {
+      console.log(this.$route)
+    },
+    async getAgentData() {
+      var dataGetter = new AgentData(this.agent_registration_no)
+      var sales = await dataGetter.getSales()
+      console.log(sales)
+      var profile = await dataGetter.getProfile()
+      console.log(profile)
+    }
+  }
 }
 
 </script>
