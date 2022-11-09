24d28e20821e0d0bafca0cc987f49f2d664ee018
Samuel Chung <41113285+schungzhihuai@users.noreply.github.com>
2022-11-09 21:08:29 +0800

    Update fb.js

--- src/scripts/fb.js
+++ src/scripts/fb.js
@@ -5,13 +5,13 @@ import { getDatabase } from "firebase/database";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    apiKey: "AIzaSyC4nJSyqPu0ZKggErnBSHkV4EpZ_VDTlIM",
-    authDomain: "propertyplanners-93ebc.firebaseapp.com",
-    projectId: "propertyplanners-93ebc",
-    storageBucket: "propertyplanners-93ebc.appspot.com",
-    messagingSenderId: "237227354592",
-    appId: "1:237227354592:web:595502638d0fa13df6e385",
-    measurementId: "G-LXFZL31XJW"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
   };
 
 // Initialize Firebase
