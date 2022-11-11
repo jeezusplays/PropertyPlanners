11a69ac67906fe1be0a01fe878732ee2f069ec28
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2022-11-12 00:23:01 +0800

    Created payment page

--- src/views/PaymentPage.vue
+++ src/views/PaymentPage.vue
@@ -1,169 +1,129 @@
 <template>
-    <div class="container-fluid">
+    <div class="container bg-light d-md-flex align-items-center mt-5">
+        <div class="row row-cols-1 row-cols-md-2">
+            <div class="col shadow-sm p-md-5 p-md-5 p-4">
 
-        <div class="row">
-            <div class="col-12">
-                <!--Insert Nav Bar-->    
-            </div>
-        </div>
-
-        <div class="row">
-            <h2>Payment</h2>
-
-            <div class="col-12">
-                <!--Insert Sidebar-->
-            </div>
-
-            <div class="col-12 col-lg-6"> 
-                <div class="card">
-                    <div class="card-body">
-                    <h3>Pay with card</h3>
-                    <h4>Credit Card Information</h4>
-
-                    <p class="card-text">
-                        <div class="form-outline mb-4">
-                            <div class="form-outline">
-                            <label class="form-label" for="card-number">Card Number</label>
-                            <input type="text" id="card-number" maxlength="19" class="form-control form-control-md"
-                            v-model="cardNumber" placeholder="1234 0000 0000 0000" pattern="[0-9\s]" inputmode="numeric"/>
-                            </div>
-                        </div>
-                        <div class="row mb-4">
-                            <div class="col-8">
-                                <label class="form-label" for="expiry">Expiration</label>
-                                <input type="text" id="expiry" class="form-control form-control-md"
-                                v-model="expiryDate" placeholder="MM / YYYY"/>
-                            </div>
-
-                            <div class="col-4">
-                                <div class="form-outline">
-                                <label class="form-label" for="cvv">CVV</label>
-                                <input type="text" id="cvv" class="form-control form-control-md"
-                                v-model="cvv" placeholder="CVV"/>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-12">
-                                <label class="form-label" for="cardholder-name">Name</label>
-                                <input type="text" id="cardholder-name" class="form-control form-control-md"
-                                v-model="cardName" placeholder="Name on Card"/>
-                            </div>
-                        </div>
-
-                        <div class="row pt-3">
-                            <div class="col-12">
-                                <p>
-                                    <input type="checkbox" v-model="isSelected">
-                                    Save this information for future purchases
-                                </p>
-                            </div>
-                        </div>
-                    </p>
+                <div class="h5 fw-bold mb-4">
+                    Monthly Plan
                 </div>
-            </div>
 
-            <div class="col-12 col-lg-6 right">
-                <div class="card border-0">
-                    <div class="card-body">
-                        
-                        <p class="card-text">
-                            <div class="row d-flex align-items-center mb-5">
-                                <div class="col-4 ps-5"><img src="../assets/virtual_currency.svg" alt="Currency" class="img-thumb"></div>
-                                <div class="col-8">
-                                    <h3>
-                                        <span class="text-success">$50</span>
-                                        Online Currency
-                                    </h3>
-                                </div>
-                            </div><hr>
-
-                            <div class="row d-flex align-items-center">
-                                <div class="col-6 text-center">Subtotal</div>
-                                <div class="col-6 text-center">$50.00</div>
-                            </div><hr>
-
-                            <div class="row py-3 d-flex align-items-center">
-                                <div class="col-6 text-center">Total</div>
-                                <h4 class="col-6 text-center">$50.00</h4>
-                            </div>
-                        </p>
+                <div class="d-flex flex-column">
+                    <div class="d-flex align-items-center justify-content-between text mt-4 mb-4">
+                        <span class="fw-semibold">Plan</span>
+                        <span class="fw-semibold">Quantity</span>
+                        <span class="fw-semibold">Price</span>
                     </div>
+                    <div class="border-bottom"></div>
+                </div>
 
-                    <div class="row text-center d-flex align-items-center">
-                        <div class="col-6">
-                            <a href="" class="text-decoration-none text-success">Back to cart</a>
-                        </div>
-                        <h4 class="col-6">
-                            <button class="btn btn-success btn-md">
-                                Place Order
-                                <span class="fas fa-arrow-right ps-2"></span></div>
-                            </button>
-                        </h4>
+                <div class="d-flex flex-column">
+                    <div class="d-flex align-items-center justify-content-between text mt-4 mb-4">
+                        <span>{{plan}}</span>
+                        <span><input style="width:20%" type="number" v-model="quantity"/></span>
+                        <span>${{price}}</span>
                     </div>
-                    
+                    <div class="border-bottom mb-4"></div>
+                    <div class="d-flex align-items-center justify-content-between text mb-4">
+                        <span>Total</span>
+                        <span>${{totalPrice}}</span>
+                    </div>
+                    <div class="border-bottom mb-4"></div>
+                    <div class="d-flex flex-column mb-4">
+                        <span>Order ID:</span>
+                        <span>{{order}}</span>
+                    </div>
+                    <div class="d-flex flex-column mb-5">
+                        <span>Days Left:</span>
+                        <span>{{daysleft}}</span>
                     </div>
                 </div>
             </div>
 
+            <div class="col shadow-sm">
+                <div class="d-flex align-items-center justify-content-between p-md-5 p-4"> 
+                    <span class="h5 fw-bold m-0">Payment</span>
+                </div>
+                <ul class="nav nav-tabs mb-3 px-md-4 px-2">
+                    <li class="nav-item"> <a class="nav-link px-2 active" aria-current="page" href="#">Credit Card</a>
+                    </li>
+                    <!-- <li class="nav-item"> <a class="nav-link px-2" href="#">Mobile Payment</a> </li>
+                <li class="nav-item ms-auto"> <a class="nav-link px-2" href="#">+ More</a> </li> -->
+                </ul>
+                <!-- <div class="px-md-5 px-4 mb-4 d-flex align-items-center">
+                <div class="btn btn-success me-4">
+                    <span class="fas fa-plus"></span>
+                </div>
+                <div class="btn-group" role="group" aria-label="Basic radio toggle button group"> 
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked> 
+                    <label class="btn btn-outline-primary" for="btnradio1"><span class="pe-1">+</span>5949</label> 
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off"> 
+                    <label class="btn btn-outline-primary" for="btnradio2"><span class="lpe-1">+</span>3894</label> 
+                </div>
+            </div> -->
+                <form action="">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="d-flex flex-column px-md-5 px-4 mb-4"> <span>Credit Card</span>
+                                <div class="inputWithIcon">
+                                    <input class="form-control" type="text" placeholder="5136 1845 5468 3894">
+                                    <span class="">
+                                        <img src="https://www.freepnglogos.com/uploads/mastercard-png/mastercard-logo-logok-15.png"
+                                            alt="" style="width:100px;">
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="d-flex flex-column ps-md-5 px-md-0 px-4 mb-4"> 
+                                <span>Expiration Date</span>
+                                <div class="inputWithIcon"> <input type="text" class="form-control" placeholder="05/20"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="d-flex flex-column pe-md-5 px-md-0 px-4 mb-4"> 
+                                <span>CVV</span>
+                                <div class="inputWithIcon"> 
+                                    <input type="password" class="form-control" placeholder="123">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="d-flex flex-column px-md-5 px-4 mb-4"> 
+                                <span>Name</span>
+                                <div> 
+                                    <input class="form-control text-uppercase" type="text" placeholder="Gold D. Roger"> 
+                                    <!-- <span><font-awesome-icon icon="fa-solid fa-user"></font-awesome-icon></span> -->
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 px-md-5 px-4 mt-3 mb-3">
+                            <div class="btn btn-primary w-100">Pay ${{totalPrice}}</div>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-
     </div>
-
 </template>
 
 <script>
 export default {
-  name: "PaymentPage",
-  data() {
-    return {
-        cardNumber : null,
-        cardName : null,
-        expiryDate : null,
-        cvv : null,
-        isSelected : false,
+    name: "Payment",
+    data () {
+        return {
+            plan: "Monthly Advertising Plan",
+            quantity: 1,
+            price: 100,
+            order: null
         }
     },
-    head: {
-        link: [
-        { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.0.7/css/all.css'}
-        ]
+    computed: {
+        totalPrice() {
+            return (this.quantity * this.price).toFixed(2)
+        },
+        daysleft() {
+            return this.quantity * 30
+        }
     }
-};
+}
 </script>
-
-<style>
-    .btn.btn-success:hover .fas {
-    transform: translateX(20px);
-    transition: transform 1.0s ease
-    }
-
-    .btn.btn-success{
-        width: 70%;
-    }
-
-    img {
-        width: 100px;
-        height: 100px;
-    }
-
-    .right {
-        background-color: rgb(241, 241, 241);
-    }
-
-    .right .card {
-        background-color: rgb(241, 241, 241);
-    }
-
-    input[type=text] {
-        width: 80%;
-        transition: width 0.7s ease-in-out;
-        box-sizing: border-box;
-    }
-
-    input[type=text]:focus {
-        width: 100%;
-        background-color: rgb(241, 241, 241)
-    }
-</style>
