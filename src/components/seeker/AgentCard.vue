ba9b159ad98b9d1266f11d4dd977cb8b6cf11b23
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2022-11-12 20:01:41 +0800

    Update agent cards in search to component

---
+++ src/components/seeker/AgentCard.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="col" v-for="agent in agents" :key="agent.id">
+        <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
+                role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#55595c" />
+            </svg>
+
+            <div class="card-body">
+                <h5 class="card-title">{{ agent.name }}</h5>
+                <p class="card-text">
+                    <i>{{ agent.location }}</i> Featured Agent <br>
+                    {{ agent.company }} <br>
+                    Something Else
+                </p>
+                <div class="d-flex justify-content-around align-items-center">
+                    <div class="btn-group w-100">
+                        <button type="button" class="btn btn-lg btn-outline-success">View</button>
+                        <button type="button" class="btn btn-lg btn-outline-secondary">Chat</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AgentCard",
+    data() {
+        return {
+            agents: [
+                {
+                    name: "Agent Name",
+                    location: "Location",
+                    company: "Company",
+                    // TODO: add image
+                },
+                {
+                    name: "Agent Name",
+                    location: "Location",
+                    company: "Company",
+                    // TODO: add image
+                },
+                {
+                    name: "Agent Name",
+                    location: "Location",
+                    company: "Company",
+                    // TODO: add image
+                },
+                {
+                    name: "Agent Name",
+                    location: "Location",
+                    company: "Company",
+                },
+                {
+                    name: "Agent Name",
+                    location: "Location",
+                    company: "Company",
+                    // TODO: add image
+                },
+                {
+                    name: "Agent Name",
+                    location: "Location",
+                    company: "Company",
+                    // TODO: add image
+                },
+                {
+                    name: "Agent Name",
+                    location: "Location",
+                    company: "Company",
+                    // TODO: add image
+                },
+                {
+                    name: "Agent Name",
+                    location: "Location",
+                    company: "Company",
+                    // TODO: add image
+                },
+            ]
+        };
+    },
+};
+</script>
--- src/components/seeker/Search.vue
+++ src/components/seeker/Search.vue
@@ -28,33 +28,10 @@
 
     <div class="album py-5 bg-light">
         <div class="container">
-
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
-                <div class="col" v-for="agent in agents" :key="agent.id">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
-                            xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
-                            preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" />
-                        </svg>
-
-                        <div class="card-body">
-                            <h5 class="card-title">{{agent.name}}</h5>
-                            <p class="card-text">
-                                <i>{{agent.location}}</i> Featured Agent <br>
-                                {{agent.company}} <br>
-                                Something Else
-                            </p>
-                            <div class="d-flex justify-content-around align-items-center">
-                                <div class="btn-group w-100">
-                                    <button type="button" class="btn btn-lg btn-outline-success">View</button>
-                                    <button type="button" class="btn btn-lg btn-outline-secondary">Chat</button>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
+                <AgentCard v-if="agents.length == 0"/>
+                <div v-else>
+                    test
                 </div>
             </div>
         </div>
@@ -62,60 +39,17 @@
 </template>
 
 <script>
+import AgentCard from "./AgentCard.vue"
+
 export default {
     name: "Search",
     data() {
         return {
-            agents: [
-                {
-                    name: "Agent Name",
-                    location: "Location",
-                    company: "Company",
-                    // TODO: add image
-                },
-                {
-                    name: "Agent Name",
-                    location: "Location",
-                    company: "Company",
-                    // TODO: add image
-                },
-                {
-                    name: "Agent Name",
-                    location: "Location",
-                    company: "Company",
-                    // TODO: add image
-                },
-                {
-                    name: "Agent Name",
-                    location: "Location",
-                    company: "Company",
-                },
-                {
-                    name: "Agent Name",
-                    location: "Location",
-                    company: "Company",
-                    // TODO: add image
-                },
-                {
-                    name: "Agent Name",
-                    location: "Location",
-                    company: "Company",
-                    // TODO: add image
-                },
-                {
-                    name: "Agent Name",
-                    location: "Location",
-                    company: "Company",
-                    // TODO: add image
-                },
-                {
-                    name: "Agent Name",
-                    location: "Location",
-                    company: "Company",
-                    // TODO: add image
-                },
-            ]
+            agents: []
         };
     },
+    components: {
+        AgentCard
+    }
 };
 </script>
